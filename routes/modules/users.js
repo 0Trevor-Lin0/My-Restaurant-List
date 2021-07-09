63d25f6ea7fd6107ef9a01c75a10f70d256702d6
Trevor Lin <trevor0966@gmail.com>
2021-07-09 16:31:38 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
+
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/login', (req, res) => {
 })
 
 module.exports = router
