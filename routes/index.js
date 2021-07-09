2bd448577b53d2aa221d1e58f8795ea478e7f4a3
Trevor Lin <trevor0966@gmail.com>
2021-07-09 14:09:14 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sorts = require('./modules/sorts')
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sorts', sorts)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
